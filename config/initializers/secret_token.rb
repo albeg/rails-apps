af6011b9ecdc2fa00b051210596ec5d05d2f950a
alol <albeg_06@mail.ru>
2013-09-05 20:02:35 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rapps::Application.config.secret_token = '7e34947bea41b595d22481c3144695ae35bedd78a96d210835797dca17884e5c57fa3ef7730efb140701d1a6c5aa7dbe7680ac842a7d4da93e7c6474fd752709'
